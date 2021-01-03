e452c52360d727043d27769d01cf7a1343d4330b
Kaleab Girma <se.kaleab.girma@gmail.com>
2021-01-03 09:19:41 +0300

    Created week1 folder. Submited reverse numbers

---
+++ Week 1/ReverseNumbers.py
@@ -0,0 +1,29 @@
+# 123 ~ 321.  -123 ~ -321. 120 ~ 21. 0 ~ 0
+# range [-2^31, 2^31-1]
+
+def reverse(x):
+    string = str(x)
+    signed = False
+    if x == 0: 
+        return 0
+    if x > (2**31 - 1) or x < (0-2**31):
+        return 0
+    if string == '0':
+        return 0
+    if '-' in string:
+        string = string[1:]
+        signed = True
+    #remove all zeros.
+    while string[-1] == '0':
+        string = string[:-1]
+    #now reverse it and add sign if it was negative.
+    reversedNum = string[::-1]
+    if signed == True:
+        reversedNum = '-' + reversedNum
+    if int(reversedNum) > (2**31 - 1) or int(reversedNum) < (0-2**31):
+        return 0
+    else:
+        return int(reversedNum)
+    
+
+print (reverse(0))
