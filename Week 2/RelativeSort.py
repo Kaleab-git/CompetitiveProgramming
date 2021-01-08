8ff21ed671a9b311749c75f9791225cc1cd4e48d
Physix <61460429+Kaleab-git@users.noreply.github.com>
2021-01-09 00:20:02 +0300

    submitted(Anagram, array intersection, relative sort, Mark and toys)

---
+++ Week 2/RelativeSort.py
@@ -0,0 +1,15 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        result = []
+        for i in range(len(arr2)):
+            for j in range(len(arr1)):
+                if arr1[j]==arr2[i]:
+                    result.append(arr2[i])
+        temp = []
+        for i in range(len(arr1)):
+            if arr1[i] not in result:
+                temp.append(arr1[i])
+        temp.sort()
+        
+        result += temp
+        return result
