27f1e31d3b45398761f9f50d3479357e260a1c0e
Kaleab Girma <se.kaleab.girma@gmail.com>
2021-01-08 10:09:47 +0300

    Constant time sorting with sleepSort. Credit: Anonymous post on 4chan

---
+++ Week 2/SleepSort.py
@@ -0,0 +1,15 @@
+import subprocess
+import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+A = [5,4,2,3,6,2,1,1]
+# The threads will call this function
+def sleepSort(num):
+    time.sleep(num/50) # workss well for num, num/10 to num/100. But for num/100 there are a few inaccuracies. Especially if the numbers are close enough 
+    return num
+
+with ThreadPoolExecutor() as thread_executor:
+    # Await all results
+    await_results = [thread_executor.submit(sleepSort, num=i) for i in A]
+    for f in as_completed([future for future in await_results]):
+        print(f.result())
