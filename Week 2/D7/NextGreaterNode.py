778dba4ef2318639a3dc5c8feb815ca3a3b92d18
Kaleab Girma <se.kaleab.girma@gmail.com>
2021-01-18 20:56:17 +0300

    Create NextGreaterNode.py

---
+++ Week 2/D7/NextGreaterNode.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def nextLargerNodes(self, head):
+        i = head; j = head
+        answer = []
+        while i != None:
+            local_max = i.val
+            while j != None:
+                if j.val > local_max:
+                    local_max = j.val
+                j = j.next
+            if local_max == i.val:
+                answer.append(0)
+            else:
+                answer.append(local_max)
+            i = i.next
+            j = i        
+        
