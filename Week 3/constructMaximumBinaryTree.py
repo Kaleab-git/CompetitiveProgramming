3a5b9a2e4fccd3f588a031383856d4ac129e8252
Kaleab Girma <se.kaleab.girma@gmail.com>
2021-01-30 15:18:00 +0300

    Create constructMaximumBinaryTree.py

---
+++ Week 3/constructMaximumBinaryTree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        if nums == []:
+            return None
+        root = TreeNode(max(nums))
+        i = nums.index(root.val)
+        root.left = self.constructMaximumBinaryTree(nums[:i])
+        root.right = self.constructMaximumBinaryTree(nums[i+1:])
+        return root
