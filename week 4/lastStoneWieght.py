7dc0ec66b976e7352792dcf5795da577fde35953
Kaleab Girma <se.kaleab.girma@gmail.com>
2021-02-05 16:24:53 +0300

    .

---
+++ week 4/lastStoneWieght.py
@@ -0,0 +1,16 @@
+import heapq as heap
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        myHeap = []
+        for stone in stones:
+            heap.heappush(myHeap, -stone)
+                
+        while len(myHeap) > 1:
+            y = -heap.heappop(myHeap); x = -heap.heappop(myHeap)
+            if x != y:
+                heap.heappush(myHeap, x-y)
+        
+        if len(myHeap) == 0:
+            return 0
+        else:
+            return -myHeap[0]
--- week 4/validBST.py
+++ week 4/validBST.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def isValidBST(self, root, Min=-2147483649, Max=2147483648):
         if root:
