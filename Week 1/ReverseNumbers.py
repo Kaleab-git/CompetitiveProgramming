9a4f7862a604fffaaa6aa0ac7ddfe4f2ad0e8caa
Kaleab Girma <se.kaleab.girma@gmail.com>
2021-02-21 22:54:50 +0300

    fixed duplicate work in my code

--- Week 1/ReverseNumbers.py
+++ Week 1/ReverseNumbers.py
@@ -4,19 +4,17 @@
 def reverse(x):
     string = str(x)
     signed = False
-    if x == 0: 
+    if x == 0:
         return 0
     if x > (2**31 - 1) or x < (0-2**31):
-        return 0
-    if string == '0':
         return 0
     if '-' in string:
         string = string[1:]
         signed = True
-    #remove all zeros.
+    # remove all zeros.
     while string[-1] == '0':
         string = string[:-1]
-    #now reverse it and add sign if it was negative.
+    # now reverse it and add sign if it was negative.
     reversedNum = string[::-1]
     if signed == True:
         reversedNum = '-' + reversedNum
@@ -24,6 +22,3 @@ def reverse(x):
         return 0
     else:
         return int(reversedNum)
-    
-
-print (reverse(0))
