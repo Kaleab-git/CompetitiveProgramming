1c607986bd8508655d8229dbe6100823cc98093f
se.kaleab.girma@gmail.com <se.segni.adeba@gmail.com>
2021-04-26 10:28:32 +0300

    4SumIIwithDictionary.py

---
+++ DailyQuestions/4SumII.py
@@ -0,0 +1,16 @@
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        # nums1[i] + nums2[j] + nums3[k] + nums[l] == 0 
+        # is means 
+        # nums1[i] + nums2[j] = -(nums3[k] + nums[l])
+        # first_sum = -(second_sum)
+        count = 0
+        first_sum = {}
+        for i in nums1:
+            for j in nums2:
+                if (i+j) in first_sum: first_sum[i + j] += 1
+                else: first_sum[i + j] = 1
+        for k in nums3:
+            for l in nums4:
+                if -(k+l) in first_sum: count += first_sum[-(k+l)]
+        return count
