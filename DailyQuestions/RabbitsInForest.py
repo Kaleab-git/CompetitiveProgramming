70cb3a22cec7c6309bf18a0017c270b6938036c3
Physix <61460429+Kaleab-git@users.noreply.github.com>
2021-05-02 09:04:50 +0300

    Formatted code for Rabbits in Forest

--- DailyQuestions/RabbitsInForest.py
+++ DailyQuestions/RabbitsInForest.py
@@ -1,9 +1,9 @@
-import math
-import collections
+import math, collections
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        count = Counter(answers)
-        total = count[0]
+        count = Counter(answers); total = count[0]
         for color in count:
             if color != 0: total += (color*math.ceil(count[color]/(color+1))) + math.ceil(count[color]/(color+1))
         return total                
+        
+        
