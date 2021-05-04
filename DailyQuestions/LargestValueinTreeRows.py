9cc0407ecee78443bfa9d117b838be5e2adb4fb8
Physix <61460429+Kaleab-git@users.noreply.github.com>
2021-05-05 00:29:22 +0300

    Create LargestValueinTreeRows.py

---
+++ DailyQuestions/LargestValueinTreeRows.py
@@ -0,0 +1,16 @@
+from collections import deque
+class Solution:
+    def largestValues(self, root: TreeNode) -> List[int]:
+        if not root: return []
+        que = deque([(root, 0)])
+        ans = [(root,0)]
+        level = 0
+        while que:
+            node, depth = que.popleft()
+            if level!=depth:
+                if que: ans.append(max(max(que, key=lambda x:x[0].val),(node,depth),key=lambda x:x[0].val))
+                else: ans.append((node,depth))
+                level = depth
+            for child in [node.left, node.right]:
+                if child: que.append((child, depth+1))
+        return [x[0].val for x in ans]
